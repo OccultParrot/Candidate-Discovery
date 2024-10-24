636de63f391ab24d1f576db17a51537b9475a07e
Thomas Stemler <stemlertho@gmail.com>
2024-10-24 14:17:14 -0500

    removed the todo and filled out the interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,16 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+interface ICandidate {
+    login: string;
+    name: string;
+    email: string;
+    company: string;
+    bio: string;
+    status?: number;
+    location: string;
+
+    /**
+     * Use *this* URL as the href for the profile picture img element!
+     */
+    avatar_url: string;
+}
+
+export default ICandidate;
